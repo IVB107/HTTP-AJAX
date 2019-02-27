31f96422a3746c30ccdfc693605ebdf647b35138
IVB107 <ianvb107@gmail.com>
2019-02-27 14:03:45 -0600

    Used Get request for friends data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+import FriendsList from './components/FriendsList';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      error: ''
+    }
+  }
+
+  componentDidMount() {
+    console.log('CDM now running...');
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        console.log(res);
+        this.setState({ friends: res.data })
+      })
+      .catch(err => {
+        console.log('Error: ' + err);
+        this.setState({ error: err })
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Derps on derps, g sauce.</p>
       </div>
     );
   }
